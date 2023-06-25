baf4f110473052b25a08ff194aef51696f7a6b76
mauriciocasillas <kamusgallagher@gmail.com>
2023-06-25 01:53:36 -0600

    Layout types

---
+++ app/src/main/java/com/kamus/m4_ejercicios/ejercicio2/layouts/ConstraintLayoutActivity.kt
@@ -0,0 +1,12 @@
+package com.kamus.m4_ejercicios.ejercicio2.layouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kamus.m4_ejercicios.R
+
+class ConstraintLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_layout)
+    }
+}
---
+++ app/src/main/java/com/kamus/m4_ejercicios/ejercicio2/layouts/FrameLayoutActivity.kt
@@ -0,0 +1,12 @@
+package com.kamus.m4_ejercicios.ejercicio2.layouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kamus.m4_ejercicios.R
+
+class FrameLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frame_layout)
+    }
+}
